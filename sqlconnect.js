d56bad4c8a5def80fa8df50ba5696b098b275ca0
Hyeongwoo Lee <lijiongyu@khu.ac.kr>
2022-06-08 00:02:15 +0900

    SQL connection changed.

--- sqlconnect.js
+++ sqlconnect.js
@@ -1,60 +1,56 @@
-const mysql = require('mysql');
-const tunnel = require('tunnel-ssh');
-const fs = require('fs');
+var mysql      = require('mysql');
 require('dotenv').config({path:"./env/.env"});
-const bodyParser = require('body-parser');
-const request = require('request');
 
-const ssh_config = {
-    username: process.env.SSH_USER,
-    privateKey: fs.readFileSync('./env/labsuser.pem'),
-    host: process.env.SSH_HOST,
-    port: process.env.SSH_PORT,
-    dstHost: process.env.SSH_DB_HOST,
-    dstPort: process.env.SSH_DB_PORT,
-};
+exports.makeconnection = function() {
+  var connection = mysql.createConnection({
+    host     : process.env.DB_HOST,    // 호스트 주소
+    user     : process.env.DB_USERNAME,           // mysql user
+    password : process.env.DB_PASSWORD,       // mysql password
+    database : process.env.DB_NAME         // mysql 데이터베이스
+  });
+  connection.connect();
+  return connection;
+}
 
-exports.SQLconnection = function(sqlline) {
-    tunnel(ssh_config, (error, server) => {
-        if (error) {
-          throw error;
-        } else if (server !== null) {
-          const conn = mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME
-          });
-          conn.connect();
-          conn.query(sqlline, function(error, results, fields) {
-            conn.end();
-            server.close();
-            if (error) { console.log(error); };
-            var dataList = [];
-            for (var i in results) {
-              let line = {}
-              for(var l in results[i]) {
-                line[l] = results[i][l];
-              }
-              dataList.push(line);
-            }            
-            request.post({
-              uri: process.env.SQL_RES_URL,
-              method: 'POST',
-              body: {
-                sql: sqlline,
-                resdata: dataList,
-                Authorization: process.env.TOKEN
-              },
-              data : 1000,
-              json:true
-            }, function (error, response, body) {
+exports.sendSQL = function(connection, sqlline, dataset) {
+  connection.query(sqlline, 
+  function (error, results, fields) {
+    if (error) throw error;
+    for(var i in results) {
+      datad = {};
+      for(var l in results[i]) {
+        datad[l] = results[i][l];
+      }
+      dataset.push(datad);
+    } // call by reference를 위해 array이용
+  });
+}
 
-            });
-            
-            
-          })
-          
+exports.sendSQL_update = function(connection, sqlline) {
+  connection.query(sqlline, 
+  function (error, results, fields) {
+    if (error) throw error;    
+  });
+}
+    
+
+exports.sendSQL_att = function(connection, sqlline, dataset, subject_id) {
+  connection.query(sqlline, 
+    function (error, results, fields) {
+      if (error) throw error;
+      console.log(results);
+      var dataList = [];
+      for(var i in results) {
+        datad = {};
+        for(var l in results[i]) {
+          datad[l] = results[i][l];
         }
+        dataList.push(datad);
+      } // call by reference를 위해 array이용
+      dataset[subject_id] = dataList;
     });
+}
+
+exports.endconnection = function(connection) {
+  connection.end();
 }
